654eda52e014df8219e0f35a9025c64ea01e9db2
Yaroslav <yaroslav@webquali.com>
2014-11-17 16:53:56 +0200

    add homework 5

---
+++ home5/HomeWork5/ASSettingViewController.h
@@ -0,0 +1,18 @@
+//
+//  ASSettingViewController.h
+//  HomeWork5
+//
+//  Created by Yaroslav Gorkun on 17.11.14.
+//  Copyright (c) 2014 yaroslav Horkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASSettingViewController : UITableViewController
+
+
+
+
+
+
+@end
---
+++ home5/HomeWork5/ASSettingViewController.m
@@ -0,0 +1,28 @@
+//
+//  ASSettingViewController.m
+//  HomeWork5
+//
+//  Created by Yaroslav Gorkun on 17.11.14.
+//  Copyright (c) 2014 yaroslav Horkun. All rights reserved.
+//
+
+#import "ASSettingViewController.h"
+
+@interface ASSettingViewController ()
+
+@end
+
+@implementation ASSettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
