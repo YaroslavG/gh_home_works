a3cf44e5d68599d4c8c9b96c8f880d0cc0aaa2af
Yaroslav <yaroslav@webquali.com>
2014-12-15 17:34:09 +0200

    add lessons 8

---
+++ GeekHub_lessons8/HomeTask8/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  homeWork3
+//
+//  Created by Yaroslav Gorkun on 03.12.14.
+//  Copyright (c) 2014 Yaroslav Gorkun. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeekHub_lessons8/HomeTask8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  homeWork3
+//
+//  Created by Yaroslav Gorkun on 03.12.14.
+//  Copyright (c) 2014 Yaroslav Gorkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
